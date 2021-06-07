edf8e11041e2ca4bffea5a2034aa4674212aede2
Edgar Guaman <edgar.guaman@finantech.com.ec>
2021-06-07 18:21:38 -0500

    Changes in Handler class

--- src/handler/dataHandler.java
+++ src/handler/dataHandler.java
@@ -3,7 +3,10 @@ package handler;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
+
+import classes.Employee;
 
 public class dataHandler {
 	
@@ -17,21 +20,23 @@ public class dataHandler {
 	        obtainedData.add(fila);
 	      }
 	      scnReader.close();
-	      System.out.println(obtainedData);
 	    } catch (FileNotFoundException e) {
 	      e.printStackTrace();
 	    }
+	splitData(obtainedData);
 	return obtainedData;
 	}
 	
 	public static void splitData(ArrayList<String> information) {
-		
 		int i;
+		HashMap<String, String>timeworked = new HashMap<String,String>();
 		for(i=0;i<information.size();i++) {
-		System.out.println("Nombre:");
-		System.out.println(information.get(i).substring(0, information.get(i).indexOf("=")));
-		System.out.println("Intervalos de trabajo:");
-		System.out.println(information.get(i).substring(information.get(i).indexOf("=")+1));
+			Employee Employee = new Employee();
+			Employee.setName(information.get(i).substring(0, information.get(i).indexOf("=")));
+			System.out.println("Nombre del empleado:"+Employee.getName());
+			//Employee.setWorktime(worktime);
+			System.out.println("Intervalos de trabajo:");
+			System.out.println(information.get(i).substring(information.get(i).indexOf("=")+1,information.get(i).indexOf("=")+3));
 		}
 	}
 	
