82d4dcc1d4e51460342d3b95c9e71bc23693f607
Edgar Guaman <edgar.guaman@finantech.com.ec>
2021-06-09 03:55:45 -0500

    End of dataHandler development

--- src/handler/dataHandler.java
+++ src/handler/dataHandler.java
@@ -3,7 +3,6 @@ package handler;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Scanner;
 
 public class dataHandler {
@@ -24,18 +23,50 @@ public class dataHandler {
 	splitData(obtainedData);
 	}
 	
-	private static HashMap<String, String> splitData(ArrayList<String> information) {
+	private static void splitData(ArrayList<String> information) {
 		int i;
 		ArrayList<String> employeeNames = new ArrayList<String>();
 		ArrayList<String> daysAndRanges = new ArrayList<String>();
-		HashMap<String, String> employeesAndRanges = new HashMap<String, String>();
 		
 		for(i=0;i<information.size();i++) {
-			String[] a = information.get(i).split("=");
-			employeeNames.add(a[0]);
-			daysAndRanges.add(a[1]);
-			employeesAndRanges.put(employeeNames.get(i), daysAndRanges.get(i));
+			String[] aux = information.get(i).split("=");
+			employeeNames.add(aux[0]);
+			daysAndRanges.add(aux[1]);
 		}
-		return employeesAndRanges;
+		
+		weekWorked(daysAndRanges);
+	}
+	
+	private static void weekWorked(ArrayList<String> dataArray) {
+		int j,k;
+		ArrayList<String> workedRanges = new ArrayList<String>();
+		for(j=0;j<dataArray.size();j++) {
+			String[] aux = dataArray.get(j).split(",");
+			for(k=0;k<aux.length;k++) {
+				workedRanges.add(aux[k]);
+			}
+		}
+		daysWorked(workedRanges);
+		rangeWorked(workedRanges);
+	}
+	
+	public static ArrayList<String> daysWorked(ArrayList<String> dataArray) {
+		int l;
+		ArrayList<String> weekdays = new ArrayList<String>();
+		
+		for(l=0;l<dataArray.size();l++) {
+			weekdays.add(dataArray.get(l).substring(0, 2));
+		}
+		return weekdays;
+	}
+	
+	public static ArrayList<String> rangeWorked(ArrayList<String> dataArray) {
+		int m;
+		ArrayList<String> timerange = new ArrayList<String>();
+		
+		for(m=0;m<dataArray.size();m++) {
+			timerange.add(dataArray.get(m).substring(2));
+		}
+		return timerange;
 	}
 }
